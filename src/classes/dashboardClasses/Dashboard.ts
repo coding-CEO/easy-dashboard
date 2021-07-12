b9a50cb728bdcb5f602e182d6b11cc5f34674751
Pratik Raut <pratikraut0000@gmail.com>
2021-07-12 15:19:57 +0530

    Created Dashboard Related Classes

---
+++ src/classes/dashboardClasses/Dashboard.ts
@@ -0,0 +1,13 @@
+import { Graph } from "./Graph";
+
+export class Dashboard {
+  public id: string = "";
+  public name: string = "";
+  public graphs: Graph[] = [];
+
+  constructor(id?: string, name?: string, graphs?: Graph[]) {
+    if (id) this.id = id;
+    if (name) this.name = name;
+    if (graphs) this.graphs = graphs;
+  }
+}
