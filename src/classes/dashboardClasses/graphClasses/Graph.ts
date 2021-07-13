9badac9cddcf056a34c6b8933bef09f3adf24a08
Pratik Raut <pratikraut0000@gmail.com>
2021-07-13 16:57:34 +0530

    Created Graph Base Class

--- src/classes/dashboardClasses/graphClasses/Graph.ts
+++ src/classes/dashboardClasses/graphClasses/Graph.ts
@@ -1,2 +1,29 @@
-//TODO: complete this later..
-export class Graph {}
+import { ApiType, GraphType } from "../../../utils/enums";
+
+export class Graph {
+  public type: GraphType;
+  public name: string;
+  public apiType: ApiType;
+  public apiUrl: string;
+  public colorHex: string;
+  public xCoordinatePath: string;
+  public yCoordinatePath: string;
+
+  constructor(
+    type: GraphType,
+    name: string,
+    apiType: ApiType,
+    apiUrl: string,
+    colorHex: string,
+    xCoordinatePath: string,
+    yCoordinatePath: string
+  ) {
+    this.type = type;
+    this.name = name;
+    this.apiType = apiType;
+    this.apiUrl = apiUrl;
+    this.colorHex = colorHex;
+    this.xCoordinatePath = xCoordinatePath;
+    this.yCoordinatePath = yCoordinatePath;
+  }
+}
